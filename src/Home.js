ca1550110fff356c2edcf64c191b44e2ab405dd5
SAMALA VIGNESH <samalavignesh@gmail.com>
2024-09-09 23:08:50 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <head>
-        <title>
-          welcome to sam website
-        </title>
-      </head>
-      <body>
-        <h1 align="center">
-          Hi sam
-        </h1>
-      </body>
+     home
     </div>
   )
 }
